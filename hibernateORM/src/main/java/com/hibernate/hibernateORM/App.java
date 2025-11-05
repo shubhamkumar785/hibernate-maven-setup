72c4a89a69c215898dcdcf6838b2aabe58783b43
Shubham Kumar <160641503+shubhamkumar785@users.noreply.github.com>
2025-11-05 23:54:59 +0530

    initital commit

---
+++ hibernateORM/src/main/java/com/hibernate/hibernateORM/App.java
@@ -0,0 +1,34 @@
+package com.hibernate.hibernateORM;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Project Started...");
+        SessionFactory factory = new Configuration().configure("hibernate.cfg.xml").buildSessionFactory();
+        
+        // Creating Student
+        Student st = new Student();
+        st.setId(101);
+        st.setName("Shubham");
+        st.setCity("Greater Noida");
+        System.out.println(st);
+        
+        Session session = factory.getCurrentSession();
+        Transaction txt = session.beginTransaction();
+        session.save(st);
+        txt.commit();
+
+        session.close();
+    }
+
+	
+}
